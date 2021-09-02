db0311d007acb6913057c9c1d0cde0dab2584265
Enrique Saul Ramirez Gonzalez <kikito110792@gmail.com>
2021-09-02 14:18:12 -0500

    first commit

---
+++ Muro/Models/SqlEjerciciosContext.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Muro.Models
+{
+    public partial class SqlEjerciciosContext : DbContext
+    {
+        public SqlEjerciciosContext()
+        {
+        }
+
+        public SqlEjerciciosContext(DbContextOptions<SqlEjerciciosContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=tcp:kikito110792.database.windows.net,1433;Initial Catalog=SqlEjercicios;Persist Security Info=False;User ID=kikito110792;Password=Qcvp08432()=;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Apellidos)
+                    .IsRequired()
+                    .HasMaxLength(30);
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .HasColumnName("email");
+
+                entity.Property(e => e.Nombre)
+                    .IsRequired()
+                    .HasMaxLength(30);
+
+                entity.Property(e => e.Registro)
+                    .HasColumnType("datetime")
+                    .HasColumnName("registro")
+                    .HasDefaultValueSql("(getdate())");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Muro/Models/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Muro.Models
+{
+    public partial class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string Email { get; set; }
+
+        public DateTime Cumpleanos { get; set; }
+        public DateTime? Registro { get; set; }
+
+        public double edad()
+        {
+            double edad = 0;
+
+            edad = (DateTime.Now - Cumpleanos).TotalDays/365;
+            
+            return edad;
+        }
+
+    }
+}
